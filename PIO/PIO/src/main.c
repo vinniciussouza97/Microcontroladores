828e2ca0cfdaecfe9aa6b48deea368fb4557b5fa
vinniciussouza97 <vinniciussouza@yahoo.com.br>
2018-03-19 20:43:44 -0300

    botao

--- PIO/PIO/src/main.c
+++ PIO/PIO/src/main.c
@@ -1,6 +1,8 @@
 #include <asf.h>
+
 #define LED_VERDE (1<<20)
 #define LED_AZUL (1<<19)
+#define BOTAO (1<<3)
 
 int main (void)
 {
@@ -8,14 +10,25 @@ int main (void)
 	board_init();
 	
 	PIOA->PIO_PER |= LED_VERDE|LED_AZUL;
+	PIOB->PIO_PER |= BOTAO;
 	PIOA->PIO_OER |= LED_VERDE|LED_AZUL;
-	PIOA->PIO_CODR |= LED_VERDE|LED_AZUL;
+	PIOB->PIO_ODR |= BOTAO;
+	PIOA->PIO_SODR |= LED_VERDE|LED_AZUL;
 
 	while(1)
 	{		
-		delay_ms(250);
-		PIOA->PIO_SODR |= LED_VERDE|LED_AZUL;
-		delay_ms(250);
-		PIOA->PIO_CODR |= LED_VERDE|LED_AZUL;
+//		delay_ms(250);
+//		PIOA->PIO_SODR |= LED_VERDE|LED_AZUL;
+//		delay_ms(250);
+//		PIOA->PIO_CODR |= LED_VERDE|LED_AZUL;
+		if (PIOB->PIO_PDSR & BOTAO) //"&" limpa todas as casas exceto a selecionada
+		{
+			PIOA->PIO_SODR |= LED_VERDE|LED_AZUL;
+		}
+		else
+		{
+			PIOA->PIO_CODR |= LED_VERDE|LED_AZUL;
+		}
+
 	}
 }
